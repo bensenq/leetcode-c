71ed937f302bf9b634663c197f769c4112d76702
cuiwei <chris.wei.cui@foxmail.com>
2018-05-12 18:20:49 +0800

    not finished

--- algorithms/94-Binary-Tree-Inorder-Traversal/main.c
+++ algorithms/94-Binary-Tree-Inorder-Traversal/main.c
@@ -6,6 +6,12 @@
  *     struct TreeNode *right;
  * };
  */
+#define NULL 0
+  struct TreeNode {
+      int val;
+      struct TreeNode *left;
+      struct TreeNode *right;
+  };
 int nodecount(struct TreeNode* root)
 {
         if (root == 0)
@@ -14,43 +20,78 @@ int nodecount(struct TreeNode* root)
                 return nodecount(root->left) + nodecount(root->right) + 1;
 }
 
-#define MAX(a, b) (a > b ? a : b)
-int treeheight(struct TreeNode* root)
+typedef struct 
 {
-	if (root == 0) 
-		return 0;
-	else
-		return MAX(treeheight(root->left), treeheight(root->left)) + 1;
-}
+	struct TreeNode *node;
+	int left;	/* indicate processing left or right */
+} _stack;
+
 /**
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* inorderTraversal(struct TreeNode* root, int* returnSize) {
         int pos = 0;
-	int height = treeheight(root);
         int count = nodecount(root);
 	struct TreeNode *temp;
-	struct TreeNode *parent[height];
-	int top = 0;
+	_stack stack[count];
+	int top;
         int *ret = malloc(count * sizeof(int));
         *returnSize = count;
+	int pop = 0;
+	temp = root;
+	top = -1;
 	while(1) {
-		while (temp->left != 0) {
-			parent[top++] = temp;
+
+//		/* leaf logic */
+//		if (temp->left == 0 && temp->right == 0) {
+//			ret[pos++] = temp->val;
+//			pop = 1;
+//			continue;
+//		}
+
+		/* push logic */
+		if (temp != 0) {
+			top++;
+			stack[top].node = temp;
+			stack[top].left = 1;
+		}
+		
+		/* inorder: left */
+		if (stack[top].left == 1 && temp->left != 0) {
 			temp = temp->left;
+			continue;
+		} else {
+			stack[top].left = 0;	/* left handle over */
 		}
 
-		ret[pos] = temp->val;
-		if (temp->right !=0) {
-			parent[top++] = temp;
+		/* inorder: middle */
+		ret[pos++] = temp->val;
+		
+		/* inorder: right */
+		if (temp->right != 0) {
 			temp = temp->right;
 			continue;
 		}
-		
-		temp = parent[top--];
-		if(top == -1)
-			break;
+
+		top--;
+		if (top == -1) {
+			break;	/* over */
+		} else {
+			if(stack[top].left == 0) {
+				goto pop;
+			} else {
+				ret[pos++] = stack[top].node->val;
+			}
+		}
+
+		if (stack[top].left == 0) {
+			top--;
+			if (top == -1)
+				break;
+			pop = 1;
+			temp = stack[top].node;
+		}
 	}
 
         return ret;
