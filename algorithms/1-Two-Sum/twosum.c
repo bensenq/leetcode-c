0153d5e6f68b342401193ad944040baaf8797fa3
sukihiro <chris.wei.cui@foxmail.com>
2022-02-13 00:52:49 +0800

    update 1-Two-Sum with two-pointer solution

--- algorithms/1-Two-Sum/twosum.c
+++ algorithms/1-Two-Sum/twosum.c
@@ -1,17 +1,44 @@
-#include <stdlib.h>
+/*algo: two-pointer */
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+int compare (const void * a, const void * b){
+	return ( *(int*)a - *(int*)b );
+}
 
-int* twoSum(int* nums, int numsSize, int target) {
-	int i, j;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+	int left, right, sum;
+    int i, found1, found2;
 	int *result = malloc(sizeof(int) * 2);
-	for (i = 0; i < numsSize - 1; i++)
-		for (j = i + 1; j < numsSize; j++)
-			if(nums[i] + nums[j] == target)
-				goto found;
-found:
-	result[0] = i;
-	result[1] = j;
+    int *dupNums = malloc(sizeof(int) * numsSize);
+    memcpy(dupNums, nums, numsSize*sizeof(int));
+	qsort(dupNums, numsSize, sizeof(int), compare);
+	left = 0;
+	right = numsSize-1;
+	while (left < right) { 
+		sum = dupNums[left] + dupNums[right];
+		if(sum > target) {
+			right--;
+		} else if (sum < target) {
+			left++;
+		} else {
+			break;
+		}
+	}
+    i = 0;
+    found1 = 0, found2 = 0;
+    while (i < numsSize) {
+        if (found1 && found2) break;
+        if (!found1 && nums[i] == dupNums[left]) {
+            result[0] = i;
+            found1 = 1;
+        } else if(!found2 && nums[i] == dupNums[right]) {
+            result[1] = i;
+            found2 = 1;
+        }
+        i++;
+    }
+
+    *returnSize = 2;
 	return result;
 }
