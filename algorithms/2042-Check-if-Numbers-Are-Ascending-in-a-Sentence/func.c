9565016363084f52bf18007df5212e421ee1ecd5
sukihiro <chris.wei.cui@foxmail.com>
2022-02-28 19:20:03 +0800

    update 2042-Check-if-Numbers-Are-Ascending-in-a-Sentence

--- algorithms/2042-Check-if-Numbers-Are-Ascending-in-a-Sentence/func.c
+++ algorithms/2042-Check-if-Numbers-Are-Ascending-in-a-Sentence/func.c
@@ -6,19 +6,13 @@ bool areNumbersAscending(char * s){
     int result;
     char number[3]; // enough: 2 <> 200
     int step;
-    i = 0;
+    i = 0; j = 0;
     while(s[i] != 0) {
         if (s[i] >= '0' && s[i] <= '9') {
             /* finding a number */
-            j = 0;
-            do {
-                number[j] = s[i] - '0';
-                j++; i++;
-            } while(s[i] != 0 && s[i] >= '0' && s[i] <= '9');
-            if (s[i] == 0)
-                break;
-
-            /* calculate value */
+            number[j] = s[i] - '0';
+            j++;
+        } else if (j > 0) {    /* calculate value */
             step = 1;
             value = 0;
             while (j > 0) {
@@ -26,7 +20,6 @@ bool areNumbersAscending(char * s){
                 step *= 10;
                 j--;
             }
-
             /* check value */
             if (value <= last) {
                 result = 0; //false
@@ -37,6 +30,23 @@ bool areNumbersAscending(char * s){
         }
         i++;
     }
+    
+    /* last number */
+    if (j > 0) {    /* calculate value */
+        step = 1;
+        value = 0;
+        while (j > 0) {
+            value += number[j-1]*step;
+            step *= 10;
+            j--;
+        }
+        /* check value */
+        if (value <= last) {
+            result = 0; //false
+            goto out;
+        }
+    }
+    
     result = 1; //true
 
 out:
