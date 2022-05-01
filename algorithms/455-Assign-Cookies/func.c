88d26ed9e96205949cb811fb03f6b37e786eca39
cuiwei <bensenq@gmail.com>
2022-05-01 17:35:49 +0800

    455-Assign-Cookies

---
+++ algorithms/455-Assign-Cookies/func.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+
+int compare (const void * a, const void * b){
+	return ( *(int*)a - *(int*)b );
+}
+
+int findContentChildren(int* g, int gSize, int* s, int sSize){
+    int count = 0;
+    int i = 0, j = 0;
+    qsort(g, gSize, sizeof(int), compare);
+    qsort(s, sSize, sizeof(int), compare);
+    while (i < gSize && s < sSize) {
+        if (g[i] > s[j]) {
+            j++;
+        } else {
+            count++;
+            i++;j++;
+        }
+    }
+    return count;
+}
